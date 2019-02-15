0d97d76d808f3a80b2a64ff04d5b6dae6952ad2b
Robert Schäfer <git@roschaefer.de>
2019-02-15 16:24:46 +0100

    Run initial migrations in branch `editor`
    We need unique contraints on the slug of a post. Therefore we merge the
    code from `37-full-text-search-top-bar` that is required to reproducibly
    create unique constraints and search indices.

---
+++ neo4j/migrate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+if [[ -z "${NEO4J_PASSWORD}" ]]; then
+  echo 'CALL db.index.fulltext.createNodeIndex("full_text_search",["Post"],["title", "content"]);' | cypher-shell
+else
+  echo "CALL dbms.security.changePassword('${NEO4J_PASSWORD}');" | cypher-shell --username neo4j --password neo4j
+fi
