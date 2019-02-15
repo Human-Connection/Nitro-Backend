b87952401ee29f1cc08d3e49829d4a3a32819070
Robert Schäfer <git@roschaefer.de>
2019-02-15 17:15:04 +0100

    Bootstrap seeds with neode
    * `yarn run db:seed`  does nothing
    * `yarn run db:reset` unaffected

--- src/seed/seed-db.js
+++ src/seed/seed-db.js
@@ -1,19 +1,4 @@
-import ApolloClient from 'apollo-client'
-import dotenv from 'dotenv'
-import fetch from 'node-fetch'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import Seed from './data/index'
+import Neode from 'neode';
 
-dotenv.config()
+const instance = new Neode.fromEnv();
 
-if (process.env.NODE_ENV === 'production') {
-  throw new Error('YOU CAN`T SEED IN PRODUCTION MODE')
-}
-
-const client = new ApolloClient({
-  link: new HttpLink({ uri: process.env.GRAPHQL_URI, fetch }),
-  cache: new InMemoryCache()
-})
-
-Seed(client)
