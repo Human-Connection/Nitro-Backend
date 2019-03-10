7547f1791e83341e0f944bb693d306f500b9c17f
Grzegorz Leoniec <greg@app-interactive.de>
2019-03-10 17:14:31 +0100

    Extracted types into seperate files

--- src/graphql-schema.js
+++ src/graphql-schema.js
@@ -1,14 +1,11 @@
-import fs from 'fs'
-import path from 'path'
 import userManagement from './resolvers/user_management.js'
 import statistics from './resolvers/statistics.js'
 import reports from './resolvers/reports.js'
 import posts from './resolvers/posts.js'
 import moderation from './resolvers/moderation.js'
+import schemas from './schemas'
 
-export const typeDefs =
-  fs.readFileSync(process.env.GRAPHQL_SCHEMA || path.join(__dirname, 'schema.graphql'))
-    .toString('utf-8')
+export const typeDefs = schemas
 
 export const resolvers = {
   Query: {
---
+++ src/schemas/index.js
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import path from 'path'
+import { mergeTypes } from 'merge-graphql-schemas'
+
+const loadSchema = file => {
+  return fs.readFileSync(path.join(__dirname, file)).toString('utf-8')
+}
+
+let typeDefs = [loadSchema('../schema.gql')]
+
+fs.readdirSync(__dirname).forEach(file => {
+  if (file.split('.').pop() === 'gql') {
+    typeDefs.push(loadSchema(file))
+  }
+})
+
+export default mergeTypes(typeDefs, { all: true })
