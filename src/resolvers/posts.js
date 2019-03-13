4583387230c3250436f751cc408752ae89333afe
Lulalaby <lala@staff.latias.eu>
2019-03-13 20:48:19 +0100

    Working Discord Post

--- src/resolvers/posts.js
+++ src/resolvers/posts.js
@@ -1,4 +1,5 @@
 import { neo4jgraphql } from 'neo4j-graphql-js'
+const { Client, RichEmbed } = require('discord.js')
 
 export default {
   Mutation: {
@@ -14,6 +15,33 @@ export default {
           postId: result.id
         })
       session.close()
+      
+      const discordBot = new Client()
+      discordBot.on("ready", () => {
+        discordBot.user.setStatus('online')
+        discordBot.user.setActivity('Posting feed', { type: 'PLAYING', url: process.env.APP_URL })
+        const guild = discordBot.guilds.get(process.env.DISCORD_GUILD);
+        const channel = guild.channels.get(process.env.DISCORD_CHANNEL)
+        const content = result.contentExcerpt.replace(/<\/?[^>]+(>|$)/g, "")
+        const image = null
+        if(result.image != null) {
+          image = result.image
+        }
+        const embed = new RichEmbed()
+          .setTitle(result.title)
+          .setColor(process.env.DISCORD_COLOR)
+          .setDescription(content)
+          .setAuthor(context.user.name, context.user.avatar, process.env.APP_URL + '/profile/' + context.user.slug)
+          .setURL(process.env.APP_URL + '/post/' + result.slug)
+          .addBlankField()
+          .setImage(image)
+          .setTimestamp()
+          .setFooter(process.env.PROJECT_NAME + ' (' + process.env.PROJECT_STATUS + ')',  process.env.PROJECT_ICON)
+        channel.send(embed)
+        discordBot.user.setAFK(true)
+      })
+      discordBot.login(process.env.DISCORD_TOKEN)
+      //throw new Exception()
 
       return result
     }
