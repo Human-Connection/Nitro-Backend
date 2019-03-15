ef2fff9f9418de6e9fd29b40cf42fee6e9a3b7b0
Grzegorz Leoniec <greg@app-interactive.de>
2019-03-15 12:58:39 +0100

    Queue writing of lastActiveAt every 10 seconds and save in batch

--- src/middleware/lastActiveMiddleware.js
+++ src/middleware/lastActiveMiddleware.js
@@ -1,22 +1,40 @@
-async function setLastActiveAt (context) {
-  if (!context.user) return
-  const session = context.driver.session()
-  await session.run(`
-      MATCH(u:User {id: $userId})
-      SET u.lastActiveAt = $lastActiveAt`, {
-    userId: context.user.id,
-    lastActiveAt: (new Date()).toISOString()
-  })
-  session.close()
+import values from 'lodash/values'
+
+const queue = {
+  users: {},
+  driver: null,
+  add: (context, lastActiveAt) => {
+    queue.driver = context.driver
+    queue.users[context.user.id] = {
+      id: context.user.id,
+      lastActiveAt
+    }
+  },
+  run: async () => {
+    if (!queue.driver || !values(queue.users).length) return
+
+    const users = { ...queue.users }
+    queue.users = {}
+
+    const session = queue.driver.session()
+    await session.run(`
+      UNWIND $users AS user
+      MATCH(u:User {id: user.id})
+      SET u.lastActiveAt = user.lastActiveAt`, {
+      users: values(users)
+    })
+    session.close()
+  }
 }
+setInterval(queue.run, 10000)
 
 export default {
   Mutation: async (resolve, root, args, context, info) => {
-    setLastActiveAt(context)
+    if (context.user) queue.add(context, (new Date()).toISOString())
     return resolve(root, args, context, info)
   },
   Query: async (resolve, root, args, context, info) => {
-    setLastActiveAt(context)
+    if (context.user) queue.add(context, (new Date()).toISOString())
     return resolve(root, args, context, info)
   }
 }
